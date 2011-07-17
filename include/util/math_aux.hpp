978e29018867c315727009fbdaa8e57a530d25a8
brunodea <brunodea@inf.ufsm.br>
2011-07-17 17:56:52 -0300

    arquivo com funcoes matematicas auxiliares adicionado.

---
+++ include/util/math_aux.hpp
@@ -0,0 +1,27 @@
+#ifndef _FAR_CRY_MATH_AUX_HPP_
+#define _FAR_CRY_MATH_AUX_HPP_
+
+#include "macros.h"
+
+namespace util
+{
+    inline int factorial(unsigned int num)
+    {
+        int result = 1;
+        for(unsigned int i = 1; i <= num; i++)
+            result *= i;
+        return result;
+    }
+
+    inline float radToDegree(float rad)
+    {
+        return (180.f*rad)/PI;
+    }
+
+    inline float degreeToRad(float d)
+    {
+        return (d*PI)/180.f;
+    }
+}
+
+#endif
