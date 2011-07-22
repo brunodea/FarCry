ebe4cd3c356663a5b39e744c1baa800a53ae8ea0
brunodea <brunodea@inf.ufsm.br>
2011-07-22 20:49:56 -0300

    arquivo que engloba o 'framework' adicionado.

---
+++ include/framework.h
@@ -0,0 +1,27 @@
+#ifndef _FAR_CRY_FRAMEWORK_H_
+#define _FAR_CRY_FRAMEWORK_H_
+
+#include "core/Matrix.hpp"
+#include "core/Vector.hpp"
+#include "core/matrix_functions.hpp"
+
+#include "model/Shape.h"
+#include "model/CircleShape.h"
+#include "model/LineShape.h"
+#include "model/PolygonShape.h"
+#include "model/GameObject.hpp"
+
+#include "util/collision_functions.h"
+#include "util/math_aux.hpp"
+
+#include "view/DrawableUnit.h"
+
+namespace framework
+{
+using namespace core;
+using namespace model;
+using namespace util;
+using namespace view;
+}
+
+#endif
